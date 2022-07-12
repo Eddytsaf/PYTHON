bbaca49137c5109aa95ae3be0f42e35188462b28
edouardtsafack <edouardtsafack@gmail.com>
2022-07-12 01:24:28 -0500

    added day 1 in Python

---
+++ day 1/Band Name.py
@@ -0,0 +1,7 @@
+from itertools import permutations
+
+
+print("Welcome to the Band Name Generator.")
+city=input("Which city did you grow up in?\n")
+pet=input("what is a name of your pet?\n")
+print("Your Band Name could be"+" " + city + " " +pet)
